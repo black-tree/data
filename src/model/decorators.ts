31410c3239b57267304d69d2f12ad6e4044e1ce3
Rubén Maguregui <ruben.maguregui@gmail.com>
2016-04-20 18:27:42 -0600

    feat(decorators): add decorator factories for declaring models and their fields

---
+++ src/model/decorators.ts
@@ -0,0 +1,63 @@
+import {declareModel, ModelPrototype, ModelDefinition} from "./model";
+import {createField, FieldDefinition} from "../field/field";
+import {FieldType, FieldTypeOptions} from "../field/field-type";
+import {Metadata} from "./metadata";
+
+/**
+ * Decorates a class for declaring it as a model. Calls [[declareModel]] on the
+ * target class with the provided options
+ * @param options The options to pass to the [[declareModel|declareModel()]]
+ * function
+ * @return {Function} The actual decorator function
+ */
+export function Model(options:ModelDefinition = {}) {
+  
+  return (Target) => {
+
+    if (fieldRegistry.has(Target.prototype)){
+      options.fields = fieldRegistry.get(Target.prototype);
+    }
+    declareModel(Target, options);
+    return Target;
+  }
+}
+
+/**
+ * Decorates a class property to declare it a model field.
+ * @param options The options used for defining the field
+ * @return {Function} The actual decorator function
+ * @constructor
+ */
+export function Field(options:FieldDecoratorOptions = {}) {
+
+  return (modelPrototype:ModelPrototype, propertyName) => {
+    let definition:FieldDefinition = Object.assign({name: propertyName}, options);
+    let metadata:Metadata = modelPrototype._modelMetadata_;
+
+    let field = createField(definition);
+    
+    if (metadata) {
+      metadata.addField(createField(definition));
+    } else {
+
+      let fields = fieldRegistry.get(modelPrototype);
+      if (!fields) {
+        fields = [];
+        fieldRegistry.set(modelPrototype, fields);
+      }
+      fields.push(field);
+    }
+  }
+}
+
+let fieldRegistry = new Map<any[]>();
+
+/**
+ * The options that can be passed to the [[Field]] decorator
+ */
+export interface FieldDecoratorOptions{
+
+  type?:string|FieldType;
+
+  typeOptions?:FieldTypeOptions;
+}
