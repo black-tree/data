a533d9529b36bc7e750745df25445316ca9e4aae
Rubén Maguregui <ruben.maguregui@gmail.com>
2016-04-25 11:59:18 -0600

    chore(docs): make tsdoc aware of external modules installed with JSPM

--- build/tasks/doc.js
+++ build/tasks/doc.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var typedoc = require("gulp-typedoc");
 gulp.task("doc", function() {
   return gulp
-    .src(["src/**/*.ts"])
+    .src(["src/**/*.ts", './jspm_packages/npm/*/*.d.ts'])
     .pipe(typedoc({
-      module: "amd",
+      module: "commonjs",
       target: "es6",
+      mode: 'modules',
       out: "docs/",
-      includeDeclarations: "jspm_packages/**/*.d.ts",
+      excludeExternals: true,
+      includeDeclarations: false,
       name: "WhiteAndGray JavaScript Data Library",
       ignoreCompilerErrors: true
     }))
