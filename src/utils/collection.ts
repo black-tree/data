bad6ae77dba7304a0ebd25301bd2754686a1806a
Rubén Maguregui <ruben.maguregui@gmail.com>
2016-04-27 16:29:19 -0600

    feat(collection): augments API with methods for searching items in the collection

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -1,3 +1,4 @@
+import {Filter} from "./filter";
 export class Collection<T> {
 
   protected items:T[];
@@ -68,16 +69,53 @@ export class Collection<T> {
     return this.items.lastIndexOf(item);
   }
 
-  find(callback:Function):T {
+  private compileSearchCriteria(criteria:SearchCriteria):Function {
+    if (typeof criteria === 'function') {
+      return <Function>criteria;
+    }
+    return (model) => {
+      for (let field in criteria) {
+        if (criteria.hasOwnProperty(field)) {
+          if (model[field] !== criteria[field]) {
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+  }
+  
+  filter(filter:Filter):T[] {
+    let matches = [];
+    for (let item of this.items) {
+      if (filter.matches(item)) {
+        matches.push(item);
+      }
+    }
+    return matches;
+  }
+
+  findBy(criteria:SearchCriteria):T[] {
+    let items = [];
+    let callback = this.compileSearchCriteria(criteria);
     for (let item of this.items) {
       if (callback(item)) {
-        return item;
+        items.push(item);
       }
+    }
+    return items;
+  }
+
+  findOneBy(criteria:SearchCriteria):T {
+    let i = this.findIndex(criteria);
+    if (i > -1) {
+      return this.items[i];
     }
     return undefined;
   }
 
-  findIndex(callback:Function):number {
+  findIndex(criteria:SearchCriteria):number {
+    let callback = this.compileSearchCriteria(criteria);
     let ii = this.items.length;
     let i = 0;
     for (; i < ii; i++) {
@@ -114,11 +152,17 @@ export class Collection<T> {
   getItems():T[] {
     return Array.from(this.items || []);
   }
+
+  forEach(callback:(value: T, index: number, array: T[]) => void):void {
+    this.items.forEach(callback);
+  }
   
   get length():number {
     return this.items.length;
   }
 }
+
+export type SearchCriteria = {[propertyName:string]: any}|Function;
 
 export class ItemsInsertion<T> {
 
